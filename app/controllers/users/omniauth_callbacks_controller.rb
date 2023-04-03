16b07dbeecc505b9b635ce50ba48b4dda39b9693
Carles Espuña <carlespuna@gmail.com>
2023-04-03 19:36:58 +0200

    omniauthcallbacks controller refactor

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,24 +1,22 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-
-      if @user.persisted?
-        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-        sign_in_and_redirect @user, event: :authentication
-      else
-        session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
-        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-      end
+    omniauth_callback('Google', 'google_data')
   end
 
   def github
+    omniauth_callback('Github', 'github_data')
+  end
+
+  private
+
+  def omniauth_callback(kind, session_key)
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Github'
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: kind
       sign_in_and_redirect @user, event: :authentication
     else
-      session['devise.github_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+      session["devise.#{session_key}"] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end

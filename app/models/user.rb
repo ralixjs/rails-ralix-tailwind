c43ed602cdb1196136002e4eb16a4463ed7142bf
Marc Anguera Insa <srmarc.ai@gmail.com>
2023-04-10 19:01:15 +0200

    add Facebook as new sign-in provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :rememberable,
          :validatable,
          :omniauthable,
-         omniauth_providers: [:google_oauth2, :github]
+         omniauth_providers: [:facebook, :google_oauth2, :github]
 
   has_one_attached :avatar
   has_many :articles, dependent: :destroy

b87b401c76b19440b82162c51eef9539a7311950
Marc Anguera <srmarc.ai@gmail.com>
2024-09-28 13:47:52 +0200

    `Ransack` working with `ActionText` (#38)

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,15 +2,16 @@ class Article < ApplicationRecord
   belongs_to :user
 
   has_rich_text :body
+  has_one :rich_text, class_name: 'ActionText::RichText', as: :record
 
   validates :title, :body, presence: true
   validates :title, length: { minimum: 5 }
 
   def self.ransackable_attributes(auth_object = nil)
-    ["title", "body"]
+    ["title"]
   end
 
   def self.ransackable_associations(auth_object = nil)
-    []
+    ["rich_text"]
   end
 end
